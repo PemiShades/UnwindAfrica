efd50ca0aba927a2d03be34cf642e3fa7c66b666
PemiShades <oluwapemisinawolesi@gmail.com>
2025-11-18 19:18:27 +0100

    moving to sqlite temporarily

--- config/settings.py
+++ config/settings.py
@@ -99,18 +99,26 @@ elif DEBUG:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv("DB_NAME", "unwind_prod"),
-            'USER': os.getenv("DB_USER", "unwind_user"),
-            'PASSWORD': os.getenv("DB_PASS", ""),
-            'HOST': os.getenv("DB_HOST", "127.0.0.1"),
-            'PORT': os.getenv("DB_PORT", "5432"),
-            'CONN_MAX_AGE': 600,
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.getenv("DB_NAME", "unwind_prod"),
+#             'USER': os.getenv("DB_USER", "unwind_user"),
+#             'PASSWORD': os.getenv("DB_PASS", ""),
+#             'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+#             'PORT': os.getenv("DB_PORT", "5432"),
+#             'CONN_MAX_AGE': 600,
+#         }
+#     }
 
 # --- Password validators ---
 AUTH_PASSWORD_VALIDATORS = [
